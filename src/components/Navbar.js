a13f87620d6d15ae348d8a2657fa4270b5b6128f
ChaoChi <chaochicheng696@gmail.com>
2021-03-14 22:34:38 +0800

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <a className="navItem" href="#">Textile</a>
+            <a className="navItem" href="#">Cookware</a>
+            <a className="navItem" href="#">Home Accessories</a>
+            <a className="navItem" href="#">Lighting</a>
+            <a className="navItem" href="#">Tableware</a>
+            <a className="navItem" href="#">Furniture</a>
+        </nav>
+    );
+}
